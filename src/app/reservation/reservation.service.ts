4876330dd24bf2f9a2d6c8573322386e15aac280
Mabrouk ABDELKARIM <mabrouk.abdelkarim@sofrecom.com>
2025-03-15 00:14:50 +0100

    Create hotel application

---
+++ src/app/reservation/reservation.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { Reservation } from '../models/reservation';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReservationService {
+
+  private reservations: Reservation[] = [];
+
+  constructor() {
+    let savedReservation = localStorage.getItem('reservations');
+    this.reservations = savedReservation ? JSON.parse(savedReservation) : [];
+  }
+
+  getReservations(): Reservation[] {
+    return this.reservations;
+  }
+
+  getReservation(id: string): Reservation | undefined {
+    return this.reservations.find(r => r.id === id);
+  }
+
+  addReservation(reservation: Reservation): void {
+    reservation.id = Date.now().toString();
+    this.reservations.push(reservation);
+    console.log(this.reservations);
+    localStorage.setItem('reservations', JSON.stringify(this.reservations));
+  }
+
+  deleteReservation(id: string): void {
+    let index = this.reservations.findIndex(r => r.id === id);
+    this.reservations.splice(index, 1);
+    localStorage.setItem('reservations', JSON.stringify(this.reservations));
+  }
+
+  updateReservation(id: string, reservation: Reservation): void {
+    let index = this.reservations.findIndex(r => r.id === id);
+    this.reservations[index] = reservation;
+    localStorage.setItem('reservations', JSON.stringify(this.reservations));
+  }
+
+}
